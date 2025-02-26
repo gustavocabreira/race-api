9d366c3239e02e7998757fa8e78d4a79361da1eb
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-26 16:58:56 -0300

    chore: add seeder on installing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::query()->updateOrCreate([
+            'email' => 'test@example.com',
+        ],
+        [
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'password',
