da3952879286ae01a338db6976bd3cc4c4106c9e
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-26 11:59:35 -0300

    test: it should return unprocessable entity when providing invalid payload

--- tests/Feature/Api/Driver/StoreDriverTest.php
+++ tests/Feature/Api/Driver/StoreDriverTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Driver;
+use Illuminate\Http\Response;
+use Illuminate\Support\Str;
 
 test('it should be able to store a new driver', function () {
     $model = new Driver;
@@ -19,3 +21,26 @@ test('it should be able to store a new driver', function () {
 
     $this->assertDatabaseCount($model->getTable(), 1);
 });
+
+dataset('invalid_payload', [
+    'empty name' => [
+        ['name' => ''], ['name' => 'The name field is required.'],
+    ],
+    'name with more than 255 characters' => [
+        ['name' => Str::repeat('*', 256)], ['name' => 'The name field must not be greater than 255 characters.'],
+    ],
+]);
+
+test('it should return unprocessable entity when providing invalid payload', function (array $payload, array $expectedErrors) {
+    $key = array_keys($expectedErrors);
+    $model = new Driver;
+
+    $response = $this->postJson(route('api.drivers.store'), $payload);
+
+    $response
+        ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ->assertJsonValidationErrors($key);
+
+    $this->assertDatabaseMissing($model->getTable(), $payload);
+    $this->assertDatabaseCount($model->getTable(), 0);
+})->with('invalid_payload');
