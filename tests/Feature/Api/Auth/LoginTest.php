49a993fab050d291c7ef8b741a267c2de6c67591
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-26 13:14:47 -0300

    test: it should return invalid credentials

--- tests/Feature/Api/Auth/LoginTest.php
+++ tests/Feature/Api/Auth/LoginTest.php
@@ -16,3 +16,19 @@ test('it should be able to login', function () {
         ->assertOk()
         ->assertJsonStructure(['access_token']);
 });
+
+test('it should return invalid credentials', function () {
+    $payload = [
+        'email' => 'email@email.com',
+        'password' => 'wrong password',
+    ];
+
+    User::factory()->create([
+        'email' => 'email@email.com',
+        'password' => 'password',
+    ]);
+
+    $response = $this->postJson(route('api.auth.login'), $payload);
+
+    $response->assertUnauthorized();
+});
